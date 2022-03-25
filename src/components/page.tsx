5afbf82206925ac22056b442d96f7c54a9badc97
Hao Chen <270001151@qq.com>
2022-03-25 13:14:34 +0800

    init

--- src/components/page.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const PageHeader = styled.div`
-    box-shadow: 0px 3px 12px 4px rgba(0, 0, 0, 0.12);
-`;
-
-export const PageContainer = styled.div`
-    padding: 2em 2em;
-`;
